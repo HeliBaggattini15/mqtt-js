39020d9d9ed0036d5d0082d445d4910e5b994826
HeliBaggattini15 <helimar.baggattini@universo.univates.br>
2022-10-19 00:32:15 -0300

    initial commit

---
+++ mqtt/index.js
@@ -0,0 +1,38 @@
+const mqtt = require('mqtt')
+const client = mqtt.connect('mqtt://test.mosquitto.org')
+
+var state = ''
+var connected = false
+
+client.on('connect', () => {
+    client.subscribe('proj/atomic/dev/heli/desc')
+    client.subscribe('proj/atomic/dev/heli/pwr')
+    client.subscribe('proj/atomic/dev/heli/mode')
+})
+
+client.on('message', (topic, message) => {
+    switch (topic) {
+        case 'proj/atomic/dev/heli/desc':
+            return handleConnected(message)
+        case 'proj/atomic/dev/heli/pwr':
+            return handlePwrState(message)
+        case 'proj/atomic/dev/heli/mode':
+            return handleMode(message)
+    }
+    console.log('No handler for topic %s', topic)
+})
+
+function handleConnected(message) {
+    console.log('%s conected', message)
+    connected = (message.toString() === 'true')
+}
+
+function handlePwrState(message) {
+    state = message
+    console.log('Power updated to %s', message)
+}
+
+function handleMode(message) {
+    console.log('Mode Changed to %s', message)
+    connected = (message.toString() === 'true')
+}
