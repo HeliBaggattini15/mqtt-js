fe216404a8f6a789bbb367c43f279d7a569b3886
Heli <Heli_B@users.noreply.github.com>
2022-10-24 20:29:50 -0300

    Ajuste na comunicação com o banco

--- mqtt/controller/postgres.js
+++ mqtt/controller/postgres.js
@@ -1,11 +1,13 @@
-const { Pool } = require('pg')
+const {Client} = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const pool = new Pool({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'postgres',
-  password: 'postgres',
-  port: 5432,
+const client = new Client({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
 })
 
-module.exports = { pool };
+module.exports = client;
--- mqtt/index.js
+++ mqtt/index.js
@@ -1,40 +1,43 @@
+const postgres = require('../mqtt/controller/postgres')
 const mqtt = require('mqtt')
 const client = mqtt.connect('mqtt://test.mosquitto.org')
-const helper = require('controller/dbUtilities')
 
-var state = ''
-var connected = false
+postgres.connect();
+var state = '';
+var connected = false;
 
 client.on('connect', () => {
-    client.subscribe('proj/atomic/dev/heli/desc')
-    client.subscribe('proj/atomic/dev/heli/pwr')
-    client.subscribe('proj/atomic/dev/heli/mode')
+    client.subscribe('proj/atomic/dev/heli/desc');
+    client.subscribe('proj/atomic/dev/heli/pwr');
+    client.subscribe('proj/atomic/dev/heli/mode');
 })
 
 client.on('message', (topic, message) => {
     switch (topic) {
         case 'proj/atomic/dev/heli/desc':
-            return handleConnected(message)
+            return handleConnected(message);
         case 'proj/atomic/dev/heli/pwr':
-            return handlePwrState(message)
+            return handlePwrState(message);
         case 'proj/atomic/dev/heli/mode':
-            return handleMode(message)
+            return handleMode(message);
     }
-    console.log('No handler for topic %s', topic)
+    console.log('No handler for topic %s', topic);
 })
 
 function handleConnected(message) {
-    console.log('%s conected', message)
-    connected = (message.toString() === 'true')
+    console.log('%s conected', message);
+    connected = (message.toString() === 'true');
     helper.default().insertData();
 }
 
 function handlePwrState(message) {
-    state = message
-    console.log('Power updated to %s', message)
+    state = message;
+    console.log('Power updated to %s', message);
 }
 
 function handleMode(message) {
-    console.log('Mode Changed to %s', message)
-    connected = (message.toString() === 'true')
+    console.log('Mode Changed to %s', message);
+    connected = (message.toString() === 'true');
 }
+
+postgres.query('insert into Persons values (2,\'helimas\'); ')
